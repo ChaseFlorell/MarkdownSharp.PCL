1366766e2a579763fd9a39495996cd9b8af9fc66
Chase Florell <chase.florell@gmail.com>
2014-08-12 19:56:00 -0600

    initial commit

---
+++ MarkdownSharp.PCL.Tests/Unit/MarkdownSharpTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace MarkdownSharp.PCL.Tests.Unit
+{
+    [TestFixture]
+    public class MarkdownSharpTests
+    {
+        private Markdown _markdown;
+
+        [SetUp]
+        public void Setup()
+        {
+            _markdown = new Markdown();
+        }
+
+        [Test]
+        public void ShouldCompileHeaderMarkdown()
+        {
+            // setup
+            const string original = @"#hello world#";
+            var expected = @"<p><h1>hello world</h1></p>" + Environment.NewLine;
+
+            // execute
+            var actual = _markdown.Transform(original);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ShouldCompileBoldMarkdown()
+        {
+            // setup
+            const string original = @"**hello world**";
+            var expected = @"<p><strong>hello world</strong></p>" + Environment.NewLine;
+
+            // execute
+            var actual = _markdown.Transform(original);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ShouldCompileItalicMarkdown()
+        {
+            // setup
+            const string original = @"*hello world*";
+            var expected = @"<p><em>hello world</em></p>" + Environment.NewLine;
+
+            // execute
+            var actual = _markdown.Transform(original);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ShouldAutoLinkEmailAddresses()
+        {
+            // setup
+            const string original = @"<address@domain.foo>";
+            // this is obfuscated see the `EmailEvaluator` method
+            // it changes to different encodings so we can't actually use "actual"
+            //     see the `EncodeEmailAddress` method
+            // instead we'll make sure the length is right.
+            var expected = @"<p><a href=""m&#97;&#x69;&#108;&#x74;&#x6f;:&#97;ddr&#x65;&#115;&#x73;&#64;&#100;&#111;&#109;&#x61;&#105;&#110;&#46;&#x66;o&#x6f;"">&#97;ddr&#x65;&#115;&#x73;&#64;&#100;&#111;&#109;&#x61;&#105;&#110;&#46;&#x66;o&#x6f;</a></p>" + Environment.NewLine;
+
+            // execute
+            var actual = _markdown.Transform(original);
+
+            // assert
+            Assert.That(actual.Length, new GreaterThanConstraint(200));
+        }
+
+        [Test]
+        public void ShouldAutoLinkUrls()
+        {
+            // setup
+            const string original = @"http://example.com";
+            var expected = @"<p><a href=""http://example.com"">http://example.com</a></p>" + Environment.NewLine;
+
+            // execute
+            var actual = _markdown.Transform(original);
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,17 @@
+This is a PCL version of the original [MarkdownSharp](https://code.google.com/p/markdownsharp/) project.
+
+-----
+Original ReadMe
+
+Open source C# implementation of [Markdown](http://daringfireball.net/projects/markdown/) processor, as featured on [Stack Overflow](http://stackoverflow.com/).
+
+This port is based heavily on the original [Perl 1.0.1](http://code.google.com/p/markdownsharp/source/browse/MarkdownSharpTests/source/perl/Markdown.101.pl) and [Perl 1.0.2b8](http://code.google.com/p/markdownsharp/source/browse/MarkdownSharpTests/source/perl/Markdown.102b8.pl) implementations of Markdown, with bits and pieces of the apparently much better maintained [PHP Markdown](http://michelf.com/projects/php-markdown/) folded into it. There are a few Stack Overflow specific modifications (which are all configurable, and all off by default). I'd like to ensure that this version stays within shouting distance of the Markdown "specification", such as it is...
+
+Please contribute!
+You'll need to know:
+
+ - C#
+ - Regular Expressions (I mean really freaking know them -- the original Markdown implementation was in Perl. Consider yourselves warned.)
+ - Markdown
+
+-----
